d082319ec05fd9bbba0df4e55aefa178017c080d
ZhouDaxia929 <zhoudongxu929@163.com>
2016-10-14 15:25:39 +0800

    Lenovo_2.1
    简单修改

--- src/Test2/TestJava.java
+++ src/Test2/TestJava.java
@@ -1,45 +1,67 @@
 package Test2;
-import java.io.*;
-import java.nio.charset.Charset;
-import java.util.*;
-//import java.io.File;
-//import java.io.FileInputStream;
-
-/**
- * Created by ggggg on 2016/9/28.
- */
-public class TestJava {
-    public static void main(String[] args) {
-
-        /*
-        Charset cset = new Charset.forName("ISO-8859-1");
-
-        Set<String> aliases = cset.aliases();
-        for(String alias : aliases)
-            System.out.println(alias);
-        */
-        Map<String, Charset> charsets = Charset.availableCharsets();
-        for(String name : charsets.keySet())
-            System.out.println(name);
-
-        System.out.println(System.getProperty("user.dir"));
-        System.out.println(File.separator);
-        try{
-            DataInputStream din = new DataInputStream(
-                    new BufferedInputStream(
-                            new FileInputStream("employee.dat")
-                    )
-            );
-            int i = din.read();
-            System.out.println(i);
+public class TestJava{
+    /**
+     * 对运算符排列
+     * @param a
+     * @param cur
+     */
+    public void fenPei(int[] a,int cur){
+        if(cur == 9){
+            check(a);
+            return;
         }
-        catch(FileNotFoundException e){
-            System.out.println("未找到");
-            System.exit(0);
+        for(int i = 0;i < 3; i++){
+            a[cur] = i;
+            fenPei(a,cur+1);
         }
-        catch(IOException e) {
-            System.out.println("发生错误");
-            System.exit(0);
+    }
+    /**
+     * 检查及输出
+     * @param a
+     */
+    public void check(int[] a){
+        int sum=0,shu,pre = 9,k;
+        for(int i = 8;i >=0; i--){
+            switch(a[i]){
+                case 1:
+                    shu = 0;
+                    for(k = 1;pre> i;pre--,k*=10){
+                        shu += pre*k;
+                    }
+                    sum -= shu;
+                    break;
+                case 2:
+                    shu = 0;
+                    for(k = 1;pre > i;pre--,k*=10){
+                        shu += pre*k;
+                    }
+                    sum += shu;
+                    break;
+                default:break;
+            }
         }
+        /**
+         * 输出
+         */
+        if(sum == 110)
+        {
+            for(int i = 1;i <9;i++){
+                System.out.print(i);
+                if(a[i] == 1)
+                    System.out.print('-');
+                if(a[i] == 2)
+                    System.out.print('+');
+            }
+            System.out.println("9=110");
+        }
+    }
+    /**
+     * main方法
+     * @param args
+     */
+    public static void main(String[] args){
+        int[] a = new int[9];
+        a[0] = 2;
+        new TestJava().fenPei(a,1);
     }
 }
